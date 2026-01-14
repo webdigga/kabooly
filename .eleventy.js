d60b35906684e0693d67187cf84bcbc855ca623a
David White <webdigga42@gmail.com>
2026-01-14 07:40:35 +0000

    Share image fix

--- .eleventy.js
+++ .eleventy.js
@@ -80,6 +80,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/favicon.svg");
   eleventyConfig.addPassthroughCopy("./src/web-app-manifest-192x192.png");
   eleventyConfig.addPassthroughCopy("./src/web-app-manifest-512x512.png");
+  eleventyConfig.addPassthroughCopy("./src/kabooly-social-share.png");
   
 
   // // Minify HTML
